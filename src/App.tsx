5a7cde0db40fd8ee3dcd0c3dba71c8f7bad2360e
Mike <kryptk@gmail.com>
2025-01-12 21:26:31 +0000

    docs: Update keyboard shortcut descriptions in help text

--- src/App.tsx
+++ src/App.tsx
@@ -36,10 +36,10 @@ const AppContent: React.FC = () => {
             ctrl+left/right to move<br/>
             d to delete<br/>
             i to insert<br/>
-            b to insert break<br/>
+            b to section break<br/>
             tab to (re-)suggest<br/>
-            ctrl+[1-8] to accept<br/>
-            [1-8] to accept and re-suggest
+            ctrl+[1-0] to accept<br/>
+            [1-0] to accept and re-suggest
           </div>
         </div>
       </div>
